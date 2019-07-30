9c42af6d863e84a29a78c6a42934aee589719ad4
Michelle Wu <snoopy4265@gmail.com>
2019-07-30 21:39:57 +0200

    Updates

---
+++ precache-manifest.06e0df6bca3011b414e574bb0e2124b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710ff578c6f87ff8015104c402740baf",
+    "url": "/mp3player/index.html"
+  },
+  {
+    "revision": "725f4e763983b0f0deb5",
+    "url": "/mp3player/static/css/main.5f6d5ce5.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/mp3player/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "725f4e763983b0f0deb5",
+    "url": "/mp3player/static/js/main.a483f043.chunk.js"
+  },
+  {
+    "revision": "137edc290ca4caa88b4c",
+    "url": "/mp3player/static/js/runtime~main.ff16f472.js"
+  }
+]);
